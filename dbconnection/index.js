c4997315b0606fee5ff15b1fd8d2d5236399d127
Mohammed Ahsan C <muhammedahsanc22@gmail.com>
2024-01-21 19:46:42 +0530

    second-commit

--- dbconnection/index.js
+++ dbconnection/index.js
@@ -2,7 +2,7 @@ const { connect } = require("mongoose");
 require("dotenv").config();
 const dbConnection = async () => {
   try {
-    if (await connect(process.env.DBURL))
+    if (await connect('mongodb+srv://muhammedahsanc22:oPRrl4dxnvoFRNcu@cluster0.d8xiqy7.mongodb.net/'))
       console.log("DB Connected Successfully");
     else throw new Error("ERR DB Connection");
   } catch (error) {
